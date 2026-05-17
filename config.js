05ebd028e760df01907d654e5ee77571e87f988b
gbt85768-hue <gbt85768@gmail.com>
2026-05-17 08:08:29 -0700

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ if (!gg) {
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://ThatOneGirlWithBigBootyAndBoobs2023:SendMeUrBoobsPic4535@cluster2023.bblquhi.mongodb.net/?retryWrites=true&w=majority"; // paste your own MongoDB url..
+global.mongodb = process.env.MONGODB || "mongodb+srv://ThatOneGirlWithBigBootyAndBoobs2023:SendMeUrBoobsPic4535@cluster2023.bblquhi.mongodb.net/?retryWrites=true&w=majority"; // mongodb+srv://gbt85768_db_user:<db_password>@hinata.jgr3k8o.mongodb.net/?appName=Hinata
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || ".";
 global.tenorApiKey =
